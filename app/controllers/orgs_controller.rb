fa6db52681649ef40a724f830f836bc60fe6b356
chengdh <cheng.donghui@gmail.com>
2011-04-01 11:04:03 +0800

    修改了机构显示

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -1,11 +1,10 @@
 #coding: utf-8
 class OrgsController < BaseController
   #使用http缓存数据到客户端
-  http_cache :index,:last_modified => Proc.new {|c| c.send(:last_modified,Org.order('updated_at DESC').first)},:etag => Proc.new {|c| c.send(:etag,"org_index")}
+  #http_cache :index,:last_modified => Proc.new {|c| c.send(:last_modified,Org.order('updated_at DESC').first)},:etag => Proc.new {|c| c.send(:etag,"org_index")}
   #提取所有机构
   def index
-    a = current_user
-    @orgs = Org.search(:is_active_eq => true)
+    @orgs = Org.where(:is_active => true).order('parent_id ASC,created_at DESC')
   end
   def new
     get_resource_ivar || set_resource_ivar(resource_class.new_with_config(params[resource_class.model_name.underscore.to_sym]))
